81b1a051699ea718f8ed7e90c96343a82f8cf367
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-17 21:31:54 +0200

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let resultArray = [];
+    text.split('').forEach(c => {
+        resultArray.unshift(c);
+    });
+    return resultArray.join('');
 };
 
 // Do not edit below this line
