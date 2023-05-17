4f427ede64da0c9617079827744ccdc35ae9f5ea
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-17 21:49:57 +0200

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    
+    for (const i in arr) {
+        if (!args.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
 
+    return result;
 };
 
 // Do not edit below this line
