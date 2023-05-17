360338c4838509cede3a3660a355356f35595ef0
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-17 22:18:19 +0200

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const convertToCelsius = function() {
-};
+const createConverter = function(converter) {
+  return function(value) {
+    const convertedValue = converter(value);
 
-const convertToFahrenheit = function() {
-};
+    if (Number.isInteger(convertedValue)) {
+      return convertedValue;
+    }
+  
+    return parseFloat(convertedValue.toFixed(1));
+  }
+}
 
-// Do not edit below this line
+const convertToCelsius = createConverter(
+  tempFahrenheit => (tempFahrenheit - 32) * 5 / 9
+);
+
+const convertToFahrenheit = createConverter(
+  tempCelcius => tempCelcius * 9 / 5 + 32
+)
+
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
