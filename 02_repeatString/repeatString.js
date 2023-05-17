04e5a83c432871e6a7147e995bfbd65af8ce52d5
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-17 21:27:06 +0200

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return 'ERROR'; 
+    }
 
+    if (n == 0) {
+        return "";
+    }
+
+    let result = str;
+    for (let i = 1; i < n; ++i) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
