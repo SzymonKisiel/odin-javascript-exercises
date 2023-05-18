b64d0f92db2da329fd8695d10fabe315bf660615
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-18 20:56:37 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const filtered = str
+        .toLowerCase()
+        .split("")
+        .filter(c => 
+            c != '!' &&
+            c != ' ' &&
+            c != ',' &&
+            c != '.');
+    
+    const reversed = Array.from(filtered).reverse();
+        
+    for (let i = 0; i < filtered.length; ++i) {
+        if (filtered[i] != reversed[i])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
