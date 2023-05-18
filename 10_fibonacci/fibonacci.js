a8fb168a25184a23769ee082ee165c22a7faddb7
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-18 21:09:31 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+let fibonacciNums = {
+    0: 0,
+    1: 1,
+    2: 1
+};
 
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    let y = fibonacciNums[n];
+    if (y != undefined) {
+        return y;
+    }
+
+    let oneBefore = fibonacci(n-1);
+    let twoBefore = fibonacci(n-2);
+    let result = oneBefore + twoBefore;
+    fibonacci[n] = result;
+    return result;
 };
 
 // Do not edit below this line
