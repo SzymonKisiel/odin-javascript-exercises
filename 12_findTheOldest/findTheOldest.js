df868262f6217338e107191fd7eb3b862c299e92
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-18 21:41:42 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people
+        .map(person => {
+            let age = 0;
+            if (person.yearOfDeath == undefined) {
+                let now = new Date()
+                age = now.getFullYear() - person.yearOfBirth;
+            }
+            else {
+                age = person.yearOfDeath - person.yearOfBirth;
+            }
 
+            return {
+                name: person.name,
+                yearOfBirth: person.yearOfBirth,
+                yearOfDeath: person.yearOfDeath,
+                age: age
+            };
+        })
+        .sort((a, b) => a.age - b.age).pop();
 };
 
 // Do not edit below this line
