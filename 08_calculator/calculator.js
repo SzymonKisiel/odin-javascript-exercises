8e4cb24fb2e02a7cadb41bde072eec6583afb972
SzymonKisiel <58905418+SzymonKisiel@users.noreply.github.com>
2023-05-18 20:28:25 +0200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(lhs, rhs) {
+	return lhs + rhs;
 };
 
-const subtract = function() {
-	
+const subtract = function(lhs, rhs) {
+	return lhs - rhs;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((result, item) => result * item, 1);
 };
 
-const power = function() {
-	
+const power = function(lhs, rhs) {
+  let result = lhs;
+
+  for (let i = 1; i < rhs; ++i) {
+    result *= lhs;
+  }
+
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+
+  for (let i = 2; i <= n; ++i) {
+    result *= i;
+  }
+
+	return result;
 };
 
 // Do not edit below this line
